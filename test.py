2df3ceb24e53c4eedf01e516ee4276b146c77e51
SztMar <70376289+SztMar@users.noreply.github.com>
2020-11-21 18:31:19 +0100

    Delete test.py
    Delete test.py file

--- test.py
+++
@@ -1,9 +0,0 @@
-def cos(id, **kwargs):
-    parameters = [f"{k} = ?" for k in kwargs]
-    parameters = ", ".join(parameters)
-    values = tuple(v for v in kwargs.values())
-    values += (id, )
-    print(parameters)
-    print(values)
-
-cos(1, status="ended", opis="zapamiętaj czasowniki ze strony 20")
