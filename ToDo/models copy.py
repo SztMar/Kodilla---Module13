69d110e136aa68b48919eec0914cce8c883c702b
SztMar <70376289+SztMar@users.noreply.github.com>
2020-11-21 18:30:43 +0100

    Add files via upload
    Add module 13

---
+++ ToDo/models copy.py
@@ -0,0 +1,48 @@
+import json
+import sqlite3
+from sqlite3 import Error
+
+class Todos:
+    def __init__(self):
+        try:
+            with open("todos.json", "r") as f:
+                self.todos = json.load(f)
+        except FileNotFoundError:
+            self.todos = []
+
+    def create_connection(db_file):
+        """ create a database connection to the SQLite database
+        specified by db_file
+        :param db_file: database file
+        :return: Connection object or None
+        """
+        conn = None
+        try:
+            conn = sqlite3.connect(db_file)
+            return conn
+        except sqlite3.Error as e:
+            print(e)
+        return conn
+    
+
+    def all(self):
+        return self.todos
+
+    def get(self, id):
+        return self.todos[id]
+
+    def create(self, data):
+        data.pop('csrf_token')
+        self.todos.append(data)
+
+    def save_all(self):
+        with open("todos.json", "w") as f:
+            json.dump(self.todos, f)
+
+    def update(self, id, data):
+        data.pop('csrf_token')
+        self.todos[id] = data
+        self.save_all()
+
+
+todos = Todos()
